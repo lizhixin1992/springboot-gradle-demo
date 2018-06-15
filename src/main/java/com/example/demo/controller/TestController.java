24f41eb9ff31f992f3eab6e3489a9bca8d6ec75b
lzx <lzx920911@gmail.com>
2018-06-15 17:03:29 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description:
+ * @email:
+ * @author: lizhixin
+ * @createDate: 15:22 2018/6/15
+ */
+@RequestMapping("test")
+@Controller
+public class TestController {
+
+    @RequestMapping("test1")
+    public String test(){
+        return "hello world";
+    }
+}

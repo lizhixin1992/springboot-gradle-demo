ac836c046e79e381f67731fd1373d64263316ab4
lizhixin <lzx920911@gmail.com>
2018-10-22 12:09:23 +0800

    update

--- src/main/java/com/example/demo/mq/ReturnCallBackListener.java
+++ src/main/java/com/example/demo/mq/ReturnCallBackListener.java
@@ -1,20 +1,20 @@
-//package com.example.demo.mq;
-//
-//import org.springframework.amqp.core.Message;
-//import org.springframework.amqp.rabbit.core.RabbitTemplate;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @description:
-// * @email:
-// * @author: lizhixin
-// * @createDate: 10:26 2018/6/20
-// */
-//@Component
-//public class ReturnCallBackListener implements RabbitTemplate.ReturnCallback {
-//    @Override
-//    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-//        System.out.println("收到回调");
-//        System.out.println("return--message:" + new String(message.getBody()) + ",replyCode:" + replyCode + ",replyText:" + replyText + ",exchange:" + exchange + ",routingKey:" + routingKey);
-//    }
-//}
+package com.example.demo.mq;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @description:
+ * @email:
+ * @author: lizhixin
+ * @createDate: 10:26 2018/6/20
+ */
+@Component
+public class ReturnCallBackListener implements RabbitTemplate.ReturnCallback {
+    @Override
+    public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+        System.out.println("收到回调");
+        System.out.println("return--message:" + new String(message.getBody()) + ",replyCode:" + replyCode + ",replyText:" + replyText + ",exchange:" + exchange + ",routingKey:" + routingKey);
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,36 @@
-//package com.example.demo;
-//
+package com.example.demo;
+
 //import org.junit.Test;
 //import org.junit.runner.RunWith;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
-//
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
-//public class DemoApplicationTests {
-//
+public class DemoApplicationTests {
+
 //	@Test
 //	public void contextLoads() {
 //		System.out.println("aaaaaaaaaaaa");
 //	}
-//
-//}
+
+
+    public static void main(String[] args) {
+        Predicate<Integer> predicate = y -> y > 5;
+
+        Consumer<Integer> consumer = x -> {
+            int a = x + 2;
+            System.out.println(a);
+            System.out.println(a + "_");
+        };
+
+        System.out.println(predicate.test(1));
+        if(predicate.test(1)) {
+            consumer.accept(1);
+        }
+    }
+
+}
